05c0a43ef70f41c3ed8aaa6c49017dee5ca858dd
Polar <readingworld2@gmail.com>
2024-01-28 00:43:48 -0500

    Inside the template literal, add an embedded expression that contains the day variable.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 const date = new Date();
-const formattedDate = ``;
+const formattedDate = `${day}`;
