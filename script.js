eb9af72671fc1c54dba3cd66cd76e5837b0c89cd
Polar <readingworld2@gmail.com>
2024-01-28 02:04:06 -0500

    Create a switch statement and use dateOptionsSelectElement.value as the expression.

--- script.js
+++ script.js
@@ -11,4 +11,7 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
-dateOptionsSelectElement.addEventListener("change", () => {});
+dateOptionsSelectElement.addEventListener("change", () => {
+  switch (dateOptionsSelectElement.value) {
+  }
+});
