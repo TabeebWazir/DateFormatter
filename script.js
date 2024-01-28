7828934ee27dcdd8b1f1f6774b27412b95abc580
Polar <readingworld2@gmail.com>
2024-01-28 00:59:49 -0500

    code rearrangement

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const currentDateParagraph = document.getElementById("current-date");
 const dateOptionsSelectElement = document.getElementById("date-options");
 const date = new Date();
-currentDateParagraph.textContent = formattedDate;
-const formattedDate = `${day}-${month}-${year}`;
-console.log(formattedDate);
 const day = date.getDate();
 const year = date.getFullYear();
 const hours = date.getHours();
 const month = date.getMonth() + 1;
 const minutes = date.getMinutes();
+const formattedDate = `${day}-${month}-${year}`;
+currentDateParagraph.textContent = formattedDate;
